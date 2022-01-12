1f918026084b5fa9269da7c3df327e11b5eea3fc
Andrés Castro Mamani <castroandresrm@gmail.com>
2022-01-12 16:14:19 -0400

    We install and configure Sequelize

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('uptasknode', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 33066,
+    define: {
+        timestamps: false
+    }
+});
